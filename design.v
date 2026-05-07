dd038e96892897bc00545864bf7ea47efa701424
U Shriram Bhat <shrirambhat@feserver.mirafra.com>
2026-05-07 07:12:20 +0530

    designnewupdated

--- design.v
+++ design.v
@@ -1,297 +1,240 @@
-module alu #(parameter N =4)(
-            input clk,
-            input rst,
-            input [1:0] inp_valid,
-            input mode,ce,cin,input [N-1:0] opa,opb,input [3:0]cmd,
-            output reg oflow,cout,g,l,e,err,output reg [2*N-1:0]res
-            );
-        wire signed [N-1:0] a,b;  
-        reg[1:0]count,count_1,count_2;
-        reg [N-1:0]opa_1,opb_1;
-        reg [2*N-1:0]temp;
-        reg signed [N:0] diff;
-        reg signed [N:0] sum;
-            always@(posedge clk or posedge rst)
-            begin
-              if(rst)
-              count<=2'b0;
-              else begin
-              if(cmd==4'd9)
-                count<=count+1;
-              else
-              count<=2'b0;   
-            end
-            end
-           always@(posedge clk or posedge rst)
-            begin
-              if(rst)
-              count_1<=2'b0;
-              else begin
-              if(cmd==4'd10)
-                count_1<=count_1+1;
-              else
-              count_1<=2'b0;   
-            end
-            end 
-            always @(posedge clk or posedge rst)
-            begin
-            if(rst)
-             count_2 <= 0;
-            else begin
-            if(cmd==4'd0)
-                count_2 <= count_2 + 1;
-            else
-                count_2 <= 0;
-             end
-            end
-            always@(posedge clk or posedge rst)
-            begin
-            if(rst)
-            begin
-             res <= {2*N{1'b0}};
-             cout <= 1'b0;
-             oflow <= 1'b0;
-             g <= 1'b0;
-             e <= 1'b0;
-             l <= 1'b0;
-             err <= 1'b0;
-            end
-            else begin
-            //default
-             res <= {2*N{1'b0}};
-             cout <= 1'b0;
-             oflow <= 1'b0;
-             g <= 1'b0;
-             e <= 1'b0;
-             l <= 1'b0;
-             err <= 1'b0;
-            if(ce)
-             begin
-             if(mode) begin
-             case(cmd)
-             4'd0: begin
-             if(inp_valid==3)begin
-             res<=opa+opb;
-             cout<=res[N];
-             end
-             end
-              4'd1:begin
-                 if(inp_valid==3)begin
-                     res<=opa-opb; 
-                   if(opa<opb)                          
-                     oflow<=1'b1;
-                   else
-                     oflow<=1'b0;  
-                   end
-                end  
-             4'd2:begin
-                 if(inp_valid==3) begin
-                    res<=opa+opb+cin;
-                    cout<=res[N];
-                   end
-                end
-             4'd3:begin
-                 if(inp_valid==3)begin
-                   res<=opa-opb-cin;                      
-                    if(opa<(opb+cin))                          
-                     oflow<=1'b1;
-                    else
-                     oflow<=1'b0;  
-                   end
-                end         
-             4'd4:begin
-                 if(inp_valid==1) begin
-                   res[N-1:0]<=opa+1;
-                   res[2*N-1:N] <= 0;
-                   end
-                end
-             4'd5:begin
-                 if(inp_valid==1)begin 
-                   res[N-1:0]<=opa-1;
-                   res[2*N-1:N] <= 0;
-                   end
-                end 
-            4'd6:begin
-                 if(inp_valid==2)begin
-                   res[N-1:0]<=opb+1;
-                   res[2*N-1:N] <= 0;
-                   end
-                end 
-            4'd7:begin
-                 if(inp_valid==2) begin
-                   res[N-1:0]<=opb-1;
-                   res[2*N-1:N] <= 0;
-                   end
-                end 
-            4'd8:begin
-                 if(inp_valid==3)
-                   begin
-                            if (opa == opb) begin
-                                g <= 0; l <= 0; e <= 1;
-                            end else if (opa > opb) begin
-                                 g <= 1; l <= 0; e <= 0;
-                            end else begin
-                                  g <= 0; l <= 1; e <= 0;
-                            end
-                        end
-                   end                
-             4'd9:begin
-                  if(inp_valid==3) begin 
-                   if(count == 0) begin
-                      opa_1 <= opa;
-                      opb_1 <= opb;
-                  end
-                  if(count==1)
-                  temp<= (opa_1+1)*(opb_1+1);                                     
-                    if(count==2)
-                   res<=temp; 
-                   end
-                  end  
-             4'd10:begin
-                   if(inp_valid==3)  begin                  
-                    if(count_1 == 0) begin
-                      opa_1 <= opa;
-                      opb_1 <= opb;
-                  end
-                  if(count_1==1)
-                  temp<=(opa_1<<1)*opb_1;                                     
-                   if(count_1==2)
-                   res<=temp; 
-                   end  
-                  end  
-            4'd11:begin
-                    if(inp_valid == 3) begin
-                       sum = a + b;
-                       res[N-1:0] <= sum[N-1:0];
-                       res[2*N-1:N] <= 0;
-                       cout <= sum[N];
-                       oflow <= (a[N-1] == b[N-1]) && (sum[N-1] != a[N-1]);
-                   if (sum == 0) begin
-                       g <= 0; l <= 0; e <= 1;
-                    end 
-                    else if (sum > 0) begin
-                       g <= 1; l <= 0; e <= 0;
-                    end 
-                    else begin
-                       g <= 0; l <= 1; e <= 0;
+alunew #(parameter N = 8)(
+    input CLK, RST, MODE, CE,
+    input [1:0] INP_VALID,
+    input [3:0] CMD,
+    input [N-1:0] OPA, OPB,
+    input CIN,
+    
+    output reg [2*N-1:0] RES,
+    output reg OFLOW, COUT, G, L, E, ERR
+);
+    reg [3:0] CMD_w;
+    reg [1:0] count;
+     reg  [1:0]INP_VALID_w;
+    reg [N-1:0]OPA_w,OPB_w;
+    reg CIN_w;
+    reg [N-1:0] temp_a, temp_b;
+    reg signed [N-1:0] signed_a, signed_b;
+    wire [N:0] sum_ext;
+    assign sum_ext = $signed(OPA_w) + $signed(OPB_w);
+    always @(posedge CLK) begin
+      if (MODE && (CMD == 4'd9 || CMD == 4'd10)) begin
+          if(count <2) 
+            count <= count + 1;
+          else
+            count <= 1;
+      end
+      else
+           count <=0;
+    end
+    always @(posedge CLK or posedge RST) begin
+        if (RST) begin 
+            RES   <= 0;
+            OFLOW <= 0;
+            COUT  <= 0;
+            G <= 0; 
+            L <= 0; 
+            E <= 0;
+            ERR <= 0;
+        end
+        else if (CE) begin 
+          	INP_VALID_w <= INP_VALID ;
+            OPA_w <= OPA;
+            OPB_w <= OPB;
+			CMD_w <= CMD;
+            OFLOW <= 0;
+            COUT  <= 0;
+            CIN_w <= CIN;
+            G <= 0; 
+            L <= 0; 
+            E <= 0;
+            ERR <= 0;
+            if (MODE) begin 
+              case (CMD_w)
+                    4'd0: begin
+                        if (INP_VALID_w == 2'b11)
+                            {COUT, RES[N-1:0]} <= OPA_w + OPB_w;
+                        else begin ERR <= 1; RES <= 0; end
                     end
-                   end
-                   end      
-            4'd12:begin
-                  if(inp_valid == 3) begin
-                      diff = a - b;
-                      res[N-1:0] <= diff[N-1:0];
-                      res[2*N-1:N] <= 0;
-                      cout <= diff[N];
-                      oflow <= (a[N-1] != b[N-1]) && (diff[N-1] != a[N-1]);
-                 if (diff == 0) begin
-                   g <= 0; l <= 0; e <= 1;
-                 end
-                 else if (diff > 0) begin
-                   g <= 1; l <= 0; e <= 0;
-                 end 
-                 else begin
-                  g <= 0; l <= 1; e <= 0;
-                 end
-                 end
-                  end                   
+                    4'd1: begin
+                        if (INP_VALID_w == 2'b11)
+                            RES[N-1:0] <= OPA_w - OPB_w;
+                        else begin ERR <= 1; RES <= 0; end
+                    end
+                    4'd2: begin
+                        if (INP_VALID_w == 2'b11)
+                        {COUT, RES[N-1:0]} <= OPA_w + OPB_w + CIN_w;
+                        else begin ERR <= 1; RES <= 0; end
+                    end
+                    4'd3: begin
+                        if (INP_VALID_w == 2'b11)
+                          RES[N-1:0] <= OPA_w - OPB_w - CIN_w;
+                        else begin ERR <= 1; RES <= 0; end
+                    end
+                    4'd4: begin
+                        if (INP_VALID_w[0])
+                            RES[N-1:0] <= OPA_w + 1;
+                        else begin ERR <= 1; RES <= 0; end
+                    end
+                    4'd5: begin
+                        if (INP_VALID_w[0])
+                            RES[N-1:0] <= OPA_w - 1;
+                        else begin ERR <= 1; RES <= 0; end
+                    end
+                    4'd6: begin
+                        if (INP_VALID_w[1])
+                            RES[N-1:0] <= OPB_w + 1;
+                        else begin ERR <= 1; RES <= 0; end
+                    end
+                    4'd7: begin
+                        if (INP_VALID_w[1])
+                            RES[N-1:0] <= OPB_w - 1;
+                        else begin ERR <= 1; RES <= 0; end
+                    end
+                    4'd8: begin
+                        if (INP_VALID_w == 2'b11) begin
+                            G <= (OPA_w > OPB_w);
+                            E <= (OPA_w == OPB_w);
+                            L <= (OPA_w < OPB_w);
+                        end 
+                        else begin ERR <= 1; RES <= 0; end
+                    end
+                    4'd9: begin
+                        if (INP_VALID_w == 2'b11) begin
+                          if (count == 2'd1) begin
+                                temp_a <= OPA_w + 1;
+                                temp_b <= OPB_w + 1;
+                                RES <= 'bx;
+                          end
+                          else if (count == 2'd2) begin
+                                RES <= temp_a * temp_b;
+                          end
+                          else
+                                RES <= RES;
+                        end
+                        else begin ERR <= 1; RES <= 0; end
+                    end
+                    4'd10: begin
+                        if (INP_VALID_w == 2'b11) begin
+                          if (count == 2'd1) begin
+                                temp_a <= OPA_w << 1;
+                                RES <= 'bx;
+                          end
+                          else if (count == 2'd2) begin
+                                RES <= temp_a * OPB_w;
+                          end
+                          else
+                                RES <= RES;
+                        end
+                        else begin ERR <= 1; RES <= 0; end
+                    end
+                    4'd11: begin
+                      signed_a = $signed(OPA_w);
+                      signed_b = $signed(OPB_w);
+                        if (INP_VALID_w == 2'b11) begin
+                            RES <= signed_a + signed_b;
+                            OFLOW <= (signed_a[N-1] == signed_b[N-1]) &&
+                                     (sum_ext[N] != signed_a[N-1]);
+                            G <= signed_a > signed_b;
+                            E <= signed_a == signed_b;
+                            L <= signed_a < signed_b;
+                        end 
+                        else begin ERR <= 1; RES <= 0; end
+                    end
+                    4'd12: begin
+                        signed_a = OPA_w;
+                        signed_b = OPB_w;
+                        if (INP_VALID_w == 2'b11) begin
+                            RES <= signed_a - signed_b;
+                            OFLOW <= (signed_a[N-1] != signed_b[N-1]) &&
+                                     (sum_ext[N] != signed_a[N-1]);
+                            G <= signed_a > signed_b;
+                            E <= signed_a == signed_b;
+                            L <= signed_a < signed_b;
+                        end 
+                        else begin ERR <= 1; RES <= 0; end
+                    end
+                    default: begin ERR <= 1; RES <= 0; end
                 endcase
-             end
-          else begin
-            case(cmd)
-            4'd0:begin
-                 if(inp_valid==3)begin
-                 res[N-1:0]<=opa&opb;
-                  res[2*N-1:N] <= 0;
-                   end
-                 end 
-            4'd1:begin
-                 if(inp_valid==3)begin
-                 res[N-1:0]<=~(opa&opb);
-                  res[2*N-1:N] <= 0;
-                   end
-                 end
-            4'd2:begin
-                 if(inp_valid==3)begin
-                 res[N-1:0]<=opa|opb;
-                  res[2*N-1:N] <= 0;
-                   end
-                 end
-            4'd3:begin
-                 if(inp_valid==3)begin
-                 res[N-1:0]<=~(opa|opb);
-                  res[2*N-1:N] <= 0;
-                   end
-                 end
-            4'd4:begin
-                 if(inp_valid==3)begin
-                 res[N-1:0]<=opa^opb;
-                  res[2*N-1:N] <= 0;
-                   end
-                 end
-            4'd5:begin
-                 if(inp_valid==3)begin
-                 res[N-1:0]<=~(opa^opb);
-                  res[2*N-1:N] <= 0;
-                   end
-                 end 
-            4'd6:begin
-                 if(inp_valid==1)begin
-                 res[N-1:0]<=~opa;
-                  res[2*N-1:N] <= 0;
-                   end
-                 end 
-            4'd7:begin
-                 if(inp_valid==2)begin
-                 res[N-1:0]<=~opb;
-                  res[2*N-1:N] <= 0;
-                   end
-                 end 
-            4'd8:begin
-                 if(inp_valid==1)begin
-                 res[N-1:0]<=(opa>>1);
-                  res[2*N-1:N] <= 0;
-                   end
-                 end           
-            4'd9:begin
-                 if(inp_valid==1)begin
-                 res[N-1:0]<=(opa<<1);
-                  res[2*N-1:N] <= 0;
-                   end
-                 end 
-            4'd10:begin
-                 if(inp_valid==2)begin
-                 res[N-1:0]<=(opb>>1);
-                  res[2*N-1:N] <= 0;
-                   end
-                 end 
-            4'd11:begin
-                 if(inp_valid==2)begin
-                 res[N-1:0]<=(opb<<1);
-                  res[2*N-1:N] <= 0;
-                   end
-                 end           
-            4'd12:begin
-                  if(inp_valid==3) 
-                   begin
-                     res<=(opa<<opb[$clog2(N)-1:0]) | (opa>>(N-opb[$clog2(N)-1:0]));
-                     res[2*N-1:N] <= 0;
-                    err<=|opb[N-1:($clog2(N)+1)];
-                  end
-                  end 
-            4'd13:begin
-                  if(inp_valid==3) 
-                   begin
-                    res[N-1:0]<=(opa>>opb[$clog2(N)-1:0])|(opa<<(N-opb[$clog2(N)-1:0]));
-                     res[2*N-1:N] <= 0;
-                    err<=|opb[N-1:($clog2(N)+1)];
-                  end
-                  end       
-            endcase
-          end    
             end
+            else begin 
+              case (CMD_w)
+                     4'd0: begin 
+                         if (INP_VALID_w==2'b11) 
+                            RES <= OPA_w & OPB_w; 
+                         else begin ERR<=1; RES <= 0; end
+                        end
+                    4'd1: begin
+                          if (INP_VALID_w==2'b11) 
+                            RES <= ~(OPA_w & OPB_w); 
+                          else begin ERR<=1; RES <= 0; end
+                        end
+                    4'd2: begin
+                            if (INP_VALID_w==2'b11) 
+                                RES <= OPA_w | OPB_w; 
+                            else begin ERR<=1; RES <= 0; end
+                    end
+                    4'd3: begin
+                            if (INP_VALID_w==2'b11) 
+                                RES <= ~(OPA_w | OPB_w); 
+                            else begin ERR<=1; RES <= 0; end
+                    end
+                    4'd4: begin
+                            if (INP_VALID_w==2'b11) 
+                                   RES <= OPA_w ^ OPB_w; 
+                            else begin ERR<=1; RES <= 0; end
+                    end
+                    4'd5: begin
+                            if (INP_VALID_w==2'b11) 
+                                    RES <= ~(OPA_w ^ OPB_w); 
+                            else begin ERR<=1; RES <= 0; end
+                    end
+                    4'd6: begin
+                        if (INP_VALID_w[0]) 
+                            RES <= ~OPA_w; 
+                        else begin ERR<=1; RES <= 0; end
+                    end
+                    4'd7: begin
+                        if (INP_VALID_w[1]) 
+                                RES <= ~OPB_w; 
+                        else begin ERR<=1; RES <= 0; end
+                    end
+                    4'd8: begin
+                        if (INP_VALID_w[0]) 
+                                RES <= OPA_w >> 1; 
+                        else begin ERR<=1; RES <= 0; end
+                    end
+                    4'd9: begin
+                        if (INP_VALID_w[0]) 
+                                RES <= OPA_w << 1; 
+                        else begin ERR<=1; RES <= 0; end
+                    end
+                    4'd10: begin
+                         if (INP_VALID_w[1]) 
+                            RES <= OPB_w >> 1; 
+                         else begin ERR<=1; RES <= 0; end
+                    end
+                    4'd11: begin
+                        if (INP_VALID_w[1]) 
+                            RES <= OPB_w << 1; 
+                        else begin ERR<=1; RES <= 0; end
+                    end
+                    4'd12: begin
+                        if (INP_VALID_w==2'b11)
+                            RES[N-1:0] <= (OPA_w << (OPB_w % N)) | (OPA_w >> (N - (OPB_w % N)));
+                        else begin ERR<=1; RES <= 0; end
+                    end
+                    4'd13: begin
+                        if (INP_VALID_w==2'b11)
+                         RES[N-1:0] <= (OPA_w >> (OPB_w % N)) | (OPA_w << (N - (OPB_w % N)));
+                        else begin ERR<=1; RES <= 0; end
+                     end
+                    default: begin ERR <= 1; RES <= 0; end
+                endcase
             end
-            end
-        assign a=opa;
-        assign b=opb;
-        endmodule
+        end
+        else begin
+            RES <= 0;
+        end
+    end
+endmodule
